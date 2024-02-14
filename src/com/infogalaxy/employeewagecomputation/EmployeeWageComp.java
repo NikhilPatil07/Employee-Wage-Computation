11c66b2d1bfac6996a4dc33c430772aa18f1d1ad
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-02-14 22:08:23 +0530

    [MODIFIED]:Nikhil - UC-3  Added Employee Type Check Functionality

--- src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
@@ -5,20 +5,25 @@ import java.util.Random;
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		
 		int attendance = 0;
 		int dailyWage = 0;
+		int attendance1;
+		int employee_type = 0;
+		int dailyWage1;
 		final int WAGE_PER_HR = 20;
 		final int FULL_DAY_HR = 8;
+		final int FULL_TIME_HR = 8;
+		final int PART_TIME_HR = 4;
 		
 		
 		System.out.println(" Welcome to Employee Wage Computation ");
 		
 		Random random = new Random();
-		attendance = random.nextInt(9) % 2;
+		attendance1 = random.nextInt(9) % 2;
 		
-		if(attendance == 0)
+		if(attendance1 == 0)
 		{
 			System.out.println(" Employee is Absent ");
 			
@@ -26,8 +31,21 @@ public class EmployeeWageComp {
 		else
 		{
 			System.out.println(" Employee is Present ");
-			dailyWage = WAGE_PER_HR * FULL_DAY_HR;
-			System.out.println(" Employee Wage = "+ dailyWage);
+			dailyWage1 = WAGE_PER_HR * FULL_DAY_HR;
+			System.out.println(" Employee Wage = "+ dailyWage1);
+			
+			if(employee_type == 0)
+			{
+				System.out.println(" Employee is Part Time ");
+				dailyWage1 = WAGE_PER_HR * PART_TIME_HR;
+			}
+			else
+			{
+				System.out.println(" Enployee is Full Time ");
+				dailyWage1 = WAGE_PER_HR * FULL_TIME_HR;
+				
+			}
+			System.out.println(" Employee Wage = "+dailyWage1);
 		}
 	}
 
