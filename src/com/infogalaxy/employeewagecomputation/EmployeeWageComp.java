e1901f63bd01f196e0adc2979efe9609da18089e
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-02-14 21:21:39 +0530

    [MODIFIFD]:Nikhil-UC-1 Completed for Employee Attendance

--- src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
@@ -1,11 +1,27 @@
 package com.infogalaxy.employeewagecomputation;
 
+import java.util.Random;
 public class EmployeeWageComp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int attendance = 0;
+		
 		System.out.println(" Welcome to Employee Wage Computation ");
+		
+		Random random = new Random();
+		attendance = random.nextInt(9) % 2;
+		
+		if(attendance == 0)
+		{
+			System.out.println(" Employee is Absent ");
+			
+		}
+		else
+		{
+			System.out.println(" Employee is Present ");
+		}
 	}
 
 }
