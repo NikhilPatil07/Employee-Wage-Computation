380c1584138fc7ec37db7211be056ae0e65e7afe
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-02-15 22:03:46 +0530

    [MODIFIED]:Nikhil - UC-4 Calculate the Employee Wage For using Switch Case

--- src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
@@ -7,13 +7,11 @@ public class EmployeeWageComp {
 	public static void main(String[] args) {
 
 		
-		int attendance = 0;
-		int dailyWage = 0;
+	
 		int attendance1;
 		int employee_type = 0;
-		int dailyWage1;
+		int dailyWage1 = 0;
 		final int WAGE_PER_HR = 20;
-		final int FULL_DAY_HR = 8;
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
 		
@@ -23,30 +21,39 @@ public class EmployeeWageComp {
 		Random random = new Random();
 		attendance1 = random.nextInt(9) % 2;
 		
-		if(attendance1 == 0)
 		{
-			System.out.println(" Employee is Absent ");
-			
-		}
-		else
-		{
-			System.out.println(" Employee is Present ");
-			dailyWage1 = WAGE_PER_HR * FULL_DAY_HR;
-			System.out.println(" Employee Wage = "+ dailyWage1);
-			
-			if(employee_type == 0)
+			switch(attendance1)
 			{
-				System.out.println(" Employee is Part Time ");
-				dailyWage1 = WAGE_PER_HR * PART_TIME_HR;
-			}
-			else
-			{
-				System.out.println(" Enployee is Full Time ");
-				dailyWage1 = WAGE_PER_HR * FULL_TIME_HR;
+				case 0:
+					System.out.println(" Employee is Absent ");
+				break;
 				
+				case 1:
+					System.out.println(" Employee is Present ");
+					
+					employee_type = random.nextInt(9) % 2;
+						switch(employee_type)
+						{
+							case 0:
+								System.out.println("Enployee is Part Time ");
+								dailyWage1 = WAGE_PER_HR * FULL_TIME_HR;
+							break;
+							
+							case 1:
+								System.out.println(" Employee id Full Time ");
+								dailyWage1 = WAGE_PER_HR * FULL_TIME_HR;
+							break;
+							
+							default:
+								System.out.println(" Not a Employee ");
+						}
+						System.out.println(" Employee Wage = " + dailyWage1);
+				break;
+				
+				default:
+					System.out.println(" Invalid Number ");
+	
 			}
-			System.out.println(" Employee Wage = "+dailyWage1);
 		}
 	}
-
 }
