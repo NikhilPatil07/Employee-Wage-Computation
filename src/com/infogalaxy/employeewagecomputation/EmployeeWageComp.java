d66b47a7102d26def628a79b7d39028374e8938e
NikhilPatil07 <patilnikhil181804@gmail.com>
2024-02-20 23:36:52 +0530

    [MODIFIFD]:Nikhil - UC-6 Calculated Total Wokring Hr and Days

--- src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
+++ src/com/infogalaxy/employeewagecomputation/EmployeeWageComp.java
@@ -12,6 +12,8 @@ public class EmployeeWageComp {
 		int employee_type = 0;
 		int dailyWage1 = 0;
 		int total_wage = 0;
+		int total_working_hr = 0;
+		int total_working_days = 1;
 		final int WAGE_PER_HR = 20;
 		final int FULL_TIME_HR = 8;
 		final int PART_TIME_HR = 4;
@@ -19,9 +21,9 @@ public class EmployeeWageComp {
 		
 		System.out.println(" Welcome to Employee Wage Computation ");
 		
-		for(int i = 1; i <= WORKING_DAYS; i++)
+		while(total_working_hr <= 100 && total_working_days <= 20 ) 
 		{
-		 System.out.println(" Day = "+i);	
+		 System.out.println(" Day = "+total_working_days);	
 		 Random random = new Random();
 		 attendance1 = random.nextInt(9) % 2;
 		
@@ -39,25 +41,30 @@ public class EmployeeWageComp {
 							case 0:
 								System.out.println("Enployee is Part Time ");
 								dailyWage1 = WAGE_PER_HR * PART_TIME_HR;
-							break;
+								total_working_hr += PART_TIME_HR;
+								break;
 							
 							case 1:
 								System.out.println(" Employee is Full Time ");
 								dailyWage1 = WAGE_PER_HR * FULL_TIME_HR;
-							break;
+								total_working_hr += FULL_TIME_HR;
+								break;
 							
 							default:
 								System.out.println(" Not a Employee ");
 						}
 						total_wage += dailyWage1;
 						System.out.println(" Employee Wage = " + dailyWage1);
-				break;
+						break;
 				
 				default:
 					System.out.println(" Invalid Number ");
 	
 			}
+			System.out.println(" Total Working Hr = "+total_working_hr);
 			System.out.println("...............");
+			total_working_days++;
 		}
+		System.out.println(" Total Monthly Wage = "+total_wage);
 	}
 }
